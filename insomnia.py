b268f1a1b2e05fdce79750635b6fc27ba0a5e491
ad-1 <andrewdaviesul@gmail.com>
2020-08-08 14:48:32 +0100

    updating insomnia to work with system tray - start stop methods

--- insomnia.py
+++ insomnia.py
@@ -1,63 +1,106 @@
+import sys
+from threading import Thread, Event
+from datetime import datetime
 from random import randrange
-from sys import exit, argv
 from time import sleep
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QAction, QApplication
-from PyQt5.QtWidgets import QSystemTrayIcon, QMenu
+from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QWidget
 from pynput.keyboard import Key, Controller
-from datetime import date
-import threading
 
 
-def print_caffeine():
-    with open('res/insomnia.txt', 'r') as f:
-        print(f.read())
+def log(msg):
+    print(f'{datetime.now()}: {msg}')
 
 
-def insomnia():
-    print('running insomnia on background thread...')
-    print('Creating keyboard controller...\n')
-    keyboard = Controller()
+class StoppableThread(Thread):
 
-    while True:
-        print(f"{date.today()}: doing some hard work...")
-        keyboard.press(Key.f15)
-        keyboard.release(Key.f15)
-        sleep(randrange(120))
+    def __init__(self,  *args, **kwargs):
+        super(StoppableThread, self).__init__(*args, **kwargs)
+        self._stop_event = Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
+
+
+class Insomnia:
+
+    def __init__(self):
+        self.running = False
+        self.print_caffeine()
+        self.keyboard = Controller()
+
+    def start(self):
+        if self.running:
+            log('Already working!')
+            return
+        self.running = True
+        log('Starting...')
+        t1 = StoppableThread(target=self.__do_work)
+        t1.start()
+
+    def __do_work(self):
+        log('Doing some hard work...')
+        while self.running:
+            self.keyboard.press(Key.f15)
+            self.keyboard.release(Key.f15)
+            sleep_time = randrange(60, 180)
+            sleep(sleep_time)
+
+    def stop(self):
+        self.running = False
+        log('Finishing work ...Stopped')
+
+    @staticmethod
+    def print_caffeine():
+        with open('res/insomnia.txt', 'r') as f:
+            print(f.read())
+
+
+# noinspection PyUnresolvedReferences
+class SystemTrayIcon(QSystemTrayIcon):
+
+    def __init__(self, icon: QIcon, parent=None):
+        QSystemTrayIcon.__init__(self, icon, parent)
+        self.setToolTip('Insomnia')
+        self.worker = Insomnia()
+        menu = QMenu(parent)
+        start_action = menu.addAction('Start')
+        start_action.triggered.connect(self.worker.start)
+        start_action.setIcon(icon)
+        stop_action = menu.addAction('Stop')
+        stop_action.triggered.connect(self.worker.stop)
+        exit_ = menu.addAction('Exit')
+        exit_.triggered.connect(self.__exit_process)
+        menu.addSeparator()
+        self.setContextMenu(menu)
+        self.activated.connect(self.__tray_icon_activated)
+
+    def __tray_icon_activated(self, reason):
+        if reason == self.DoubleClick:
+            log('double click detected..')
+            self.worker.start()
+
+    def __exit_process(self):
+        # TODO: Handle exit
+        self.worker.stop()
+        log('... Process Finished')
+        sys.exit()
 
 
 def main():
-    print_caffeine()
-    print('Starting app main method...')
-    app = QApplication(argv)
-
-    print('Setting up system tray icon...')
-    icon = QIcon('res/insomnia-96.png')
-    tray_icon = QSystemTrayIcon(icon, parent=app)
-    tray_icon.setToolTip('Insomnia')
+    app = QApplication(sys.argv)
+    icon = QIcon('res\\insomnia-96.png')
+    w = QWidget()
+    tray_icon = SystemTrayIcon(icon, w)
     tray_icon.show()
-
-    print('Adding menu option to exit...')
-    menu = QMenu()
-
-    exit_act = QAction(QIcon('exit.png'), '&Exit', None)
-    # noinspection PyUnresolvedReferences
-    exit_act.triggered.connect()
-    # exit_act.setShortcut('Ctrl+Q')
-
-    menu.addAction(exit_act)
-    tray_icon.setContextMenu(menu)
-
-    t1 = threading.Thread(target=insomnia)
-    t1.start()
-
-    exit(app.exec_())
-
-
-def close_app(app, t1):
-    app.quit()
+    tray_icon.showMessage('Ready To Work', 'Insomnia')
+    sys.exit(app.exec_())
 
 
 # Program driver
 if __name__ == '__main__':
+    # sys.stdout = open('res\\log.txt', 'w')
     main()
